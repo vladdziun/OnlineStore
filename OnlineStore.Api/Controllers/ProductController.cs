51a8581cbdab39ae0da7b09d1833a0bbc2f071b3
Vlad Dziun <vlad@skyhive.io>
2022-12-28 15:52:37 -0800

    first commit

---
+++ OnlineStore.Api/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopOnline.Api.Repositories.Contracts;
+using ShopOnline.Models.Dtos;
+using ShopOnline.Api.Extensions;
+
+
+namespace ShopOnline.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetItems()
+        {
+            try
+            {
+                var products = await _productRepository.GetItems();
+                var productCategories = await _productRepository.GetCategories();
+
+                if( products == null || productCategories == null ) 
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    var productDtos = products.ConvertToDto(productCategories);
+                    return Ok(productDtos);
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the DB");
+                throw;
+            }
+        }
+    }
+}
---
+++ OnlineStore.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Entities;
+using ShopOnline.Api.Repositories.Contracts;
+
+namespace ShopOnline.Api.Repositories
+{
+    public class ProductRepository: IProductRepository
+    {
+        private readonly ShopOnlineDbContext _shopOnlineDbContext;
+
+        public ProductRepository(ShopOnlineDbContext shopOnlineDbContext)
+        {
+            _shopOnlineDbContext = shopOnlineDbContext;
+        }
+        public async Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            var categories = await _shopOnlineDbContext.ProductCategories.ToListAsync();
+
+            return categories;
+        }
+
+        public async Task<ProductCategory> GetCategory(int id)
+        {
+            var category = await _shopOnlineDbContext.ProductCategories.SingleOrDefaultAsync(c => c.Id == id);
+
+            return category;
+        }
+
+        public async Task<Product> GetItem(int id)
+        {
+            //var product = await _shopOnlineDbContext.Products
+            //                    .Include(p => p.ProductCategory)
+            //                    .SingleOrDefaultAsync(p => p.Id == id);
+            //return product;
+            throw new NotImplementedException();
+
+        }
+
+        public async Task<IEnumerable<Product>> GetItems()
+        {
+            var products = await _shopOnlineDbContext.Products.ToListAsync();
+
+            return products;
+
+        }
+
+        public async Task<IEnumerable<Product>> GetItemsByCategory(int id)
+        {
+            //var products = await _shopOnlineDbContext.Products
+            //                         .Include(p => p.ProductCategory)
+            //                         .Where(p => p.CategoryId == id).ToListAsync();
+            //return products;
+            throw new NotImplementedException();
+        }
+    }
+}
